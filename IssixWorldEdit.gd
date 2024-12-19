bda8af3e6b2599256484f3d85a99c897a12187f5
Frisk <7283122+Friskygote@users.noreply.github.com>
2024-12-20 00:40:35 +0100

    Cleanup files on git

--- IssixWorldEdit.gd
+++
@@ -1,8 +0,0 @@
-extends WorldEditBase
-
-func _init():
-	id = "IssixWorldEdit"
-	
-func apply(world: GameWorld):
-	if(getFlag("IssixModule.Issix_Introduced")):
-		world.setRoomSprite("hall_ne_corner", RoomStuff.RoomSprite.PERSON)
