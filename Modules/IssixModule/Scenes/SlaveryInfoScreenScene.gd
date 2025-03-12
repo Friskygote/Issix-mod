b6d8886987831004374a180068c681dd3864a015
Frisk <git@frisk.space>
2025-03-12 21:59:11 +0100

    Fixed #25

--- Modules/IssixModule/Scenes/SlaveryInfoScreenScene.gd
+++ Modules/IssixModule/Scenes/SlaveryInfoScreenScene.gd
@@ -1155,7 +1155,7 @@ func _react(_action: String, _args):
 				_action = "azazelfertilityrepeatsex"
 
 	if _action == "azazelfertilityfirst":
-		GM.pc.getInventory().removeFirstOf("lube")
+		GM.pc.getInventory().removeXFromItemOrDelete("lube", 1)
 		GlobalRegistry.getCharacter("azazel").getInventory().addItem(GlobalRegistry.createItem("lube"))
 		setModuleFlag("IssixModule", "Had_Previously_Trained_Fertility_LVL1", true)
 
