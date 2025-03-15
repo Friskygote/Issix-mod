1fec6c6c270eb671fddef9f88a3d56f877c07b24
Frisk <git@frisk.space>
2025-03-15 12:52:50 +0100

    Cleaning up directory structure on host machine resulting in few updates I've missed when syncing

--- Modules/IssixModule/IssixWorldEdit.gd
+++ Modules/IssixModule/IssixWorldEdit.gd
@@ -4,5 +4,4 @@ func _init():
 	id = "IssixWorldEdit"
 	
 func apply(world: GameWorld):
-	if(getFlag("IssixModule.Issix_Introduced")):
-		world.setRoomSprite("hall_ne_corner", RoomStuff.RoomSprite.PERSON)
+	world.setRoomSprite("hall_ne_corner", RoomStuff.RoomSprite.PERSON)
